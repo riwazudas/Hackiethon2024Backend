d279d60e68c413622af32fa94b6f5ee73760745a
bryan sunjaya <bryansunjaya@bryans-MBP.gateway>
2024-03-24 03:08:25 +1100

    update ribabot memory

--- Submissions/ribabot.py
+++ Submissions/ribabot.py
@@ -6,10 +6,14 @@ from Game.playerActions import defense_actions, attack_actions, projectile_actio
 from Game.gameSettings import HP, LEFTBORDER, RIGHTBORDER, LEFTSTART, RIGHTSTART, PARRYSTUN
 from random import randint, random
 import itertools
+from random import sample
+from collections import deque
 # Define your Q-Learning agent class
+from collections import deque
+
 class QLearningAgent:
-    def __init__(self, num_actions, num_states, initial_epsilon=1.0, min_epsilon=0.01, decay_rate=0.995,
-                 learning_rate=0.1, discount_factor=0.9):
+    def __init__(self, num_actions, num_states, initial_epsilon=0.5, min_epsilon=0.01, decay_rate=0.995,
+                 learning_rate=0.1, discount_factor=0.3, memory_capacity=10000, batch_size=32):
         self.num_actions = num_actions
         self.num_states = num_states
         self.learning_rate = learning_rate
@@ -18,24 +22,12 @@ class QLearningAgent:
         self.min_epsilon = min_epsilon
         self.decay_rate = decay_rate
         self.q_table = [[0] * num_actions for _ in range(num_states)]
-        self.game_history = []
-        
-    def update_q_table_from_history(self, reward):
-        """
-        Update Q-table based on the game history.
-        """
-        for state, action in self.game_history:
-            self.q_table[state][action] += self.learning_rate * (reward - self.q_table[state][action])
+        self.memory = deque(maxlen=memory_capacity)
+        self.batch_size = batch_size
 
-    def clear_game_history(self):
-        """
-        Clear game history after updating Q-table.
-        """
-        self.game_history = []
-        
     def choose_action(self, state):
         if random() < self.epsilon:
-            return randint(0, self.num_actions - 1)  
+            return randint(0, self.num_actions - 1)
         else:
             return self.get_best_action(state)
 
@@ -51,16 +43,26 @@ class QLearningAgent:
     def decay_epsilon(self):
         self.epsilon = max(self.min_epsilon, self.epsilon * self.decay_rate)
 
+    def store_experience(self, state, action, reward, next_state):
+        self.memory.append((state, action, reward, next_state))
 
+    def sample_experience(self):
+        return sample(self.memory, min(len(self.memory), self.batch_size))
+
+    def learn_from_replay(self):
+        if len(self.memory) >= self.batch_size:
+            experiences = self.sample_experience()
+            for state, action, reward, next_state in experiences:
+                self.update_q_table(state, action, reward, next_state)
+                
 # PRIMARY CAN BE: Teleport, Super Saiyan, Meditate, Dash Attack, Uppercut, One Punch
 # SECONDARY CAN BE : Hadoken, Grenade, Boomerang, Bear Trap
 
 # TODO FOR PARTICIPANT: Set primary and secondary skill here
 PRIMARY_SKILL = DashAttackSkill
+#SECONDARY_SKILL = SuperSaiyanSkill
 SECONDARY_SKILL = Hadoken
 
-#constants, for easier move return
-#movements
 JUMP = ("move", (0,1))
 FORWARD = ("move", (1,0))
 BACK = ("move", (-1,0))
@@ -97,9 +99,9 @@ class Script:
     # DO NOT TOUCH
     def init_player_skills(self):
         return self.primary, self.secondary
-
+    
     def combo(self, distance, enemyblockstatus):
-        combodashattack = [BACK, JUMP_BACKWARD, SECONDARY]
+        combodashattack = [BACK, JUMP_BACKWARD, PRIMARY]
         if distance <= 5 and not enemyblockstatus:
             return combodashattack
         else:
@@ -145,25 +147,39 @@ class Script:
                             return True
             return False
 
-    def is_hit_by_consecutive_light_attacks(self, player):
-        player_last_move = get_last_move(player)
-        if player_last_move == LIGHT:  # Check if the player's last move was a light attack
+    def primary_hit(self, player, enemy):
+        if get_last_move(player) == PRIMARY and get_hp(enemy) < previous_enemy_hp:
+            return True
+        return False
+
+    def is_hit_by_consecutive_light_attacks(self, player, enemy):
+        enemy_last_move = get_last_move(enemy)
+        if enemy_last_move == LIGHT and get_hp(player) < self.previous_player_hp:  
+            # Check if the player's last move was a light attack and player's HP decreased
             self.consecutive_light_hits += 1
         else:
             self.consecutive_light_hits = 0  # Reset consecutive hit count if last move was not a light attack
         
+        self.previous_player_hp = get_hp(player)  # Update previous player's HP
+        
         return self.consecutive_light_hits > 1
+
         
     def calculate_reward(self, player, enemy, player_projectiles, enemy_projectiles):
         reward = 0
-            
+        hp_difference = get_hp(player) - get_hp(enemy)
         # Check if the game has been won by the agent
-        if self.game_won_by_agent(player, enemy):
+        if hp_difference > 0:
             reward += 50
+        else:
+            reward -= 30
+            
+        if self.game_won_by_agent(player, enemy):
+            reward += 70
                 
         # Check if damage was dealt to the opponent
         if self.damage_dealt_to_opponent(player, enemy):
-            reward += 15
+            reward += 20
                 
         # Check if the agent successfully avoided taking damage
         if self.agent_avoids_damage(player, enemy, self.previous_player_hp):
@@ -171,18 +187,21 @@ class Script:
                 
         # Check if the game was lost by the agent
         if self.game_lost_by_agent(player, enemy):
-            reward -= 30
+            reward -= 100
                 
         # Check if the agent took damage
         if self.agent_takes_damage(player, enemy):
-            reward -= 10
+            reward -= 50
                 
         # Penalize inactivity
         if self.agent_remains_inactive():
-            reward -= 2
-
-        if self.is_hit_by_consecutive_light_attacks(player):
             reward -= 5
+
+        if self.is_hit_by_consecutive_light_attacks(player,enemy):
+            reward -= 10
+
+        if self.primary_hit(player, enemy):
+            reward +=30
         return reward
 
     
@@ -192,37 +211,49 @@ class Script:
         # You need to define your own state representation
         # Example: state = 0 if player's health is higher else 1
         state = 0 if get_hp(player) > get_hp(enemy) else 1
-        #agent.decay_epsilon() 
+        agent.decay_epsilon() 
         # Choose action using Q-Learning agent
         action = agent.choose_action(state)
         self.previous_player_hp = get_hp(player)
         self.previous_enemy_hp = get_hp(enemy)
+        
         # Check for combo actions
         is_jumping = get_last_move(player) in JUMP
         is_enemy_jumping = get_last_move(enemy) in JUMP
         combo_actions = [
             ([PRIMARY], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
-            ([SECONDARY], lambda is_enemy_jumping, is_jumping: not is_enemy_jumping and not is_jumping),
             ([PRIMARY, LIGHT], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, LIGHT], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, HEAVY], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, HEAVY], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, BLOCK], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, BLOCK], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, JUMP_FORWARD], lambda distance, is_jumping: False),
             ([PRIMARY, JUMP_BACKWARD], lambda distance, is_jumping: False),
-            ([SECONDARY, JUMP_FORWARD], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and not is_jumping),
-            ([SECONDARY, JUMP_BACKWARD], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and not is_jumping),
             ([PRIMARY, PRIMARY], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, PRIMARY], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, SECONDARY], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, SECONDARY], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, CANCEL], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, CANCEL], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping),
             ([PRIMARY, NOMOVE], lambda distance, is_jumping: distance < 7 and not is_jumping),
-            ([SECONDARY, NOMOVE], lambda is_enemy_jumping, is_jumping: is_enemy_jumping and is_jumping)
         ]
+
+        if SECONDARY == SuperSaiyanSkill:
+            combo_actions = [
+                ([SECONDARY], lambda is_jumping: is_jumping and get_last_move(player) == BACK),
+                ([LIGHT], lambda distance, is_jumping: distance < 3 and not is_jumping),
+                ([LIGHT, PRIMARY], lambda distance, is_jumping: distance < 3 and not is_jumping)
+        ]
+        elif SECONDARY == Hadoken:
+            combo_actions.extend([
+                ([SECONDARY], lambda is_jumping: is_jumping),
+                ([SECONDARY], lambda is_jumping: not is_jumping),
+                ([SECONDARY, LIGHT], lambda is_jumping: is_jumping),
+                ([SECONDARY, HEAVY], lambda is_jumping: is_jumping),
+                ([SECONDARY, BLOCK], lambda is_jumping: is_jumping),
+                ([SECONDARY, JUMP_FORWARD], lambda is_jumping: not is_jumping),
+                ([SECONDARY, JUMP_BACKWARD], lambda is_jumping: not is_jumping),
+                ([SECONDARY, PRIMARY], lambda is_jumping: is_jumping),
+                ([SECONDARY, SECONDARY], lambda is_jumping: is_jumping),
+                ([SECONDARY, CANCEL], lambda is_jumping: is_jumping),
+                ([SECONDARY, NOMOVE], lambda is_jumping: is_jumping),
+            ])
+
         #action
         # Execute action based on chosen action
         if action == 0:
@@ -261,22 +292,13 @@ class Script:
         # Update Q-table if this is not the first move
         if self.previous_state is not None:
             reward = self.calculate_reward(player, enemy, player_projectiles, enemy_projectiles)
-            agent.update_q_table(self.previous_state, self.previous_action, reward, state)
-        
+            #agent.update_q_table(self.previous_state, self.previous_action, reward, state)
+            agent.store_experience(self.previous_state, self.previous_action, reward, state)
         # Store current state and action as previous state and action for the next iteration
         self.previous_state = state
         self.previous_action = action
-
+        agent.learn_from_replay()
         return move
 
-         # Update Q-table if this is not the first move
-        if self.previous_state is not None:
-            reward = self.calculate_reward(player, enemy, player_projectiles, enemy_projectiles)
-            agent.update_q_table(self.previous_state, self.previous_action, reward, state)
-        
-        # Store current state and action as previous state and action for the next iteration
-        self.previous_state = state
-        self.previous_action = action
 
-        return move
 
